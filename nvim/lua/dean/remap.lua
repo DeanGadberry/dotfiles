d6e6ad30a2301f1e960255ca7e7c643fd03226a9
DeanGadberry <dean@deangadberry.com>
2023-01-20 17:58:57 -0600

    update

--- nvim/lua/dean/remap.lua
+++ nvim/lua/dean/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.api.nvim_set_keymap("n", "<leader>y", '"+y', {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<leader>p", '"+p', {noremap=true, silent=true})
